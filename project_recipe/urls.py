9e7e62019f1aba9ef4fb142b47f9527c06221586
Artem Velychko <artemvel.ua7@gmail.com>
2026-05-22 14:18:08 +0300

    Редаговано Urls

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-
+from recipe.views import main, category_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main, name='main'),
+    path('categories/', category_list, name='category_list'),
 ]
